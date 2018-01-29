81bda35a29859756e448fd67dddf696d62541e32
刘祺 <gucong@gmail.com>
2018-01-29 11:16:51 +0800

    update deps & eslint config

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+'use strict';
 var describe = require('mocha').describe;
 // var before = require('mocha').before;
 // var after = require('mocha').after;
@@ -7,41 +8,51 @@ var assert = require('assert');
 var fs = require('mz/fs');
 var execFile = require('mz/child_process').execFile;
 var exec = require('mz/child_process').exec;
-var Promise = require('any-promise');
+
 require('../');
 
-describe('git path', function() {
-	it('ls', function() {
-		return execFile('ls').then(function(result) {
+describe('git path', function () {
+	it('ls', function () {
+		return execFile('ls').then(function (result) {
 			result = result[0].split(/\r?\n/).filter(Boolean).sort();
 			assert.ok(result.indexOf('package.json') >= 0);
 			assert.ok(result.indexOf('README.md') >= 0);
 		});
 	});
-	it('cat README.md', function() {
+	it('cat README.md', function () {
 		return Promise.all([
 			exec('cat README.md'),
 			execFile('cat', ['README.md']),
-			fs.readFile('README.md', {encoding:'utf8'}),
-		]).then(function(result) {
+			fs.readFile('README.md', {encoding: 'utf8'}),
+		]).then(function (result) {
 			assert.equal(result[0][0], result[2]);
 			assert.equal(result[1][0], result[2]);
 		});
 	});
-	it('zdiff --help', function() {
+	it('zdiff --help', function () {
 		return Promise.all([
 			exec('zdiff --help'),
 			execFile('zdiff', ['--help']),
-		]).then(function(result) {
+		]).then(function (result) {
 			assert.ok(result[0][0].indexOf('OPTIONs are the same as for') >= 0);
 			assert.deepEqual(result[0], result[1]);
 		});
 	});
-	it('eslint --help', function() {
+	it('eslint --help', function () {
+		var env = Object.assign({}, process.env, {
+			PATH: [
+				'node_modules/.bin',
+				process.env.PATH,
+			].join(';'),
+		});
 		return Promise.all([
-			exec('node_modules/.bin/eslint --help'),
-			execFile('node_modules/.bin/eslint', ['--help']),
-		]).then(function(result) {
+			exec('eslint --help', {
+				env: env,
+			}),
+			execFile('eslint', ['--help'], {
+				env: env,
+			}),
+		]).then(function (result) {
 			assert.ok(result[0][0].indexOf('Basic configuration:') >= 0);
 			assert.deepEqual(result[0], result[1]);
 		});
