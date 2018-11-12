118fe0360f0c57e66f6cfab5990cfbf40f1f403b
gucong <gucong@gmail.com>
2018-11-12 18:18:22 +0800

    debug for clink

--- bin/git-bash-shell.cmd
+++ bin/git-bash-shell.cmd
@@ -4,6 +4,12 @@
 ;= if defined npm_lifecycle_script goto:eof
 ;= set _=env
 ;= for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
+;= if not exist "%ProgramData%\Cmder\vendor\clink\clink.bat" goto:eof
+;= set "CMDER_ROOT=%ProgramData%\Cmder"
+;= setlocal enabledelayedexpansion
+;= if "%PROCESSOR_ARCHITECTURE%"=="x86" ( set "architecture=86" ) else ( set "architecture=64" )
+;= "%CMDER_ROOT%\vendor\clink\clink_x%architecture%.exe" inject --quiet --profile "%CMDER_ROOT%\config" --scripts "%CMDER_ROOT%\vendor"
+;= endlocal
 ;= goto:eof
 env=if defined SHELL ( env.exe $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" env.exe $* )
 $SHELL=if defined SHELL ( env.exe "%SHELL%" $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/bash $* )
