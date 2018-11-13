fb88f346ed626173b6e8d8e0798f1be5f93c8011
gucong <gucong@gmail.com>
2018-11-13 18:05:26 +0800

    call clink

--- bin/git-bash-shell.cmd
+++ bin/git-bash-shell.cmd
@@ -1,8 +1,8 @@
 ;= @echo off
 ;= %SystemRoot%\System32\doskey.exe /listsize=1000 /macrofile=%0%
-;= if defined _ goto:eof
 ;= if defined npm_lifecycle_script goto:eof
-;= set _=env
+;= if defined TERM goto:eof
+;= set TERM=cygwin
 ;= for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
 ;= goto:eof
 env=if defined SHELL ( env.exe $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" env.exe $* )
@@ -10,6 +10,6 @@ $SHELL=if defined SHELL ( env.exe "%SHELL%" $* ) else ( env.exe "SHELL=%GIT_INST
 bash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\bash" /bin/bash $*
 dash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\dash" /bin/dash $*
 sh=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/sh $*
-npm=env.exe TERM=cygwin npm $*
-npx=env.exe TERM=cygwin npx $*
+npm=env.exe npm $*
+npx=env.exe npx $*
 node=env.exe node $*
