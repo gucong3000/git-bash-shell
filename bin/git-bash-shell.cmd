7e66788423ef5c20a1833c72d6b60435d35ebafc
gucong <gucong@gmail.com>
2018-11-13 19:20:19 +0800

    proxy of `.npmrc`

--- bin/git-bash-shell.cmd
+++ bin/git-bash-shell.cmd
@@ -1,6 +1,7 @@
 ;= @echo off
 ;= if defined GIT_BASH_SHELL_LOCK ( goto:eof ) else ( set GIT_BASH_SHELL_LOCK=True )
 ;= %SystemRoot%\System32\doskey.exe /listsize=1000 /macrofile=%0%
+;= if defined TERM ( if defined SHLVL goto:eof )
 ;= if defined npm_lifecycle_script goto:eof
 ;= for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
 ;= set GIT_BASH_SHELL_LOCK=
