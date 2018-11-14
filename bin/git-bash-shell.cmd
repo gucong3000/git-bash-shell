88e9092d84cf49f9fcceb39f12e35a317d5e7adb
gucong <gucong@gmail.com>
2018-11-14 10:00:54 +0800

    Add doskey node.exe

--- bin/git-bash-shell.cmd
+++ bin/git-bash-shell.cmd
@@ -1,6 +1,6 @@
 ;= @echo off
-;= if defined GIT_BASH_SHELL_LOCK ( goto:eof ) else ( set GIT_BASH_SHELL_LOCK=True )
 ;= %SystemRoot%\System32\doskey.exe /listsize=1000 /macrofile=%0%
+;= if defined GIT_BASH_SHELL_LOCK ( goto:eof ) else ( set GIT_BASH_SHELL_LOCK=True )
 ;= if defined TERM ( if defined SHLVL goto:eof )
 ;= if defined npm_lifecycle_script goto:eof
 ;= for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
@@ -11,6 +11,7 @@ $SHELL=if defined SHELL ( env.exe "%SHELL%" $* ) else ( env.exe "SHELL=%GIT_INST
 bash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\bash" /bin/bash $*
 dash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\dash" /bin/dash $*
 sh=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/sh $*
+node.exe=env.exe node $*
+node=env.exe node $*
 npm=env.exe npm $*
 npx=env.exe npx $*
-node=env.exe node $*
