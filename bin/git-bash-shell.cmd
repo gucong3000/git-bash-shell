5e2a8c89ff14094f60afd591ac5e0805580c126e
gucong <gucong@gmail.com>
2018-11-15 17:36:02 +0800

    login to bash

--- bin/git-bash-shell.cmd
+++ bin/git-bash-shell.cmd
@@ -1,13 +1,16 @@
 ;= @echo off
 ;= if defined GIT_BASH_SHELL_INIT goto:eof
 ;= set GIT_BASH_SHELL_INIT=True
-;= "%SystemRoot%\System32\doskey.exe" /listsize=1000 /macrofile="%0%"
-;= for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
+;= @for /F "delims=" %%F in ('node.exe "%~dp0\..\lib\cli-environment.js"') do %%F
+;= if defined SHELL env.exe "%SHELL%" --login
+;= doskey /listsize=1000 /macrofile="%0%"
+;= if defined CMDER_ROOT "%CMDER_ROOT%\vendor\clink\clink_x64.exe" inject --autorun --quiet --profile "%CMDER_ROOT%\config" --scripts "%CMDER_ROOT%\vendor"
 ;= goto:eof
 env=if defined SHELL ( env.exe $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" env.exe $* )
-$SHELL=if defined SHELL ( env.exe "%SHELL%" $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/bash $* )
+$SHELL=if defined SHELL ( env.exe "%SHELL%" $* ) else ( env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/sh $* )
+dash=env.exe "SHELL=%GIT_INSTALL_ROOT%\usr\bin\dash" /usr/bin/dash $*
+zsh=env.exe "SHELL=%GIT_INSTALL_ROOT%\usr\bin\zsh" /usr/bin/zsh $*
 bash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\bash" /bin/bash $*
-dash=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\dash" /bin/dash $*
 sh=env.exe "SHELL=%GIT_INSTALL_ROOT%\bin\sh" /bin/sh $*
 node.exe=env.exe node $*
 node=env.exe node $*
